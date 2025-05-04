f4eec5f88a503936b712defee0ff2c9ad50a86c8
JobertDev27 <jobertdev27@gmail.com>
2025-05-04 17:09:03 +0800

    created floating header element

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router";
+import searchBtnImg from "../images/search.webp";
+import cartBtnImg from "../images/shopping-cart.webp";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <div className="header-title">
+          <h1>Minku</h1>
+        </div>
+        <div className="header-searchbar">
+          <input
+            type="search"
+            name="searchbar"
+            className="search-bar"
+            placeholder="Search..."
+          />
+          <button className="search-btn">
+            <img src={searchBtnImg} alt="search button" />
+          </button>
+        </div>
+        <nav>
+          <ul className="header-nav">
+            <li className="cart-container">
+              <Link to={"/Cart"}>
+                <span className="cart-amount">5</span>
+                <img className="cart-button" src={cartBtnImg} alt="" />
+              </Link>
+            </li>
+            <li>
+              <Link to={"/Store"}>
+                <button className="button">SHOP</button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
