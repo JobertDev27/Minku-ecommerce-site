5cc6728e066b1a145325153f3687a2674fe00572
JobertDev27 <jobertdev27@gmail.com>
2025-05-04 17:09:53 +0800

    fixed routing issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider} from 'react-router'
-import Home from './Home'
-import Store from './Store'
-import Cart from './Cart'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./Header.css";
+import Home from "./Home";
+import Store from "./Store";
+import Cart from "./Cart";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>
+    element: <Home />,
   },
   {
     path: "/Store",
-    element: <Store/>
+    element: <Store />,
   },
   {
     path: "/Cart",
-    element: <Cart/>
-  }
-])
+    element: <Cart />,
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
