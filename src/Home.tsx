ccf5af0ebe26f23c9289c6cfbf94320d72e7c037
JobertDev27 <jobertdev27@gmail.com>
2025-05-04 17:10:14 +0800

    added hero section and best seller section

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,46 @@
-import { Link } from "react-router"
+import { useEffect } from "react";
+import Header from "./components/Header";
+
+import heroImg from "./images/bag.webp";
+import sellersBg from "./images/sellersBG.svg";
 
 export default function Home() {
-    return <div>
-        <Link to={"/Store"}>Store</Link>
-        <Link to={"/Cart"}>cart</Link>
-    </div>
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const url: string = "https://dummyjson.com/products?limit=0";
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section className="hero">
+          <img className="hero-image" src={heroImg} alt="blue bag" />
+          <div className="hero-offer">
+            <p className="hero-discount">GET 50% OFF!</p>
+            <p className="hero-info">
+              Get our current deals at half the price!
+            </p>
+            <div className="hero-btn-container">
+              <button className="button">Shop Now</button>
+              <button className="sec-button">Learn More</button>
+            </div>
+          </div>
+        </section>
+        <img className="sellers-bg" src={sellersBg} alt="" />
+        <section className="best-seller">
+          <h2>BEST SELLERS</h2>
+        </section>
+      </main>
+    </>
+  );
 }
