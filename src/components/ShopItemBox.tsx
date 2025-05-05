1837c00da7827b5b6e01fbc74eded071f14a6e32
JobertDev27 <jobertdev27@gmail.com>
2025-05-05 16:19:36 +0800

    now properly displays shop products

--- src/components/ShopItemBox.tsx
+++ src/components/ShopItemBox.tsx
@@ -1,3 +1,31 @@
-export default function ShopItemBix() {
-  return <div></div>;
+type Product = {
+  title: string;
+  price: number;
+  thumbnail: string;
+};
+
+type Props = {
+  product: Product;
+  onclick: () => void;
+};
+
+export default function ShopItemBox({ product, onclick }: Props) {
+  return (
+    <div className="item-container">
+      <img
+        src={product.thumbnail}
+        alt={product.title}
+        width={256}
+        height={256}
+        className="product-thumb"
+      />
+      <div className="item-content">
+        <p className="item-name">{product.title}</p>
+        <p className="item-price">{product.price}</p>
+        <button className="add-item-button button" onClick={onclick}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
