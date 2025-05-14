91d166888a0202acf43b3d7fe8a7e85a5b5d52b0
JobertDev27 <jobertdev27@gmail.com>
2025-05-14 18:59:40 +0800

    added cart total

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -18,18 +18,36 @@ export default function Cart() {
     setCartItems(getCartData());
   }, []);
 
+  const setTotal = () => {
+    let total: number = 0;
+    cartItems.map((item) => {
+      total += item.price * item.amount;
+    });
+    return total;
+  };
+
   return (
-    <main className="cart-page">
-      <nav className="home-link-container">
-        <Link to="/" className="home-link">
-          &#60; Return to home
-        </Link>
-      </nav>
-      <div className="cart-item-grid">
-        {cartItems.map((item, index) => {
-          return <CartItem product={item} key={index} />;
-        })}
+    <>
+      <main className="cart-page">
+        <nav className="home-link-container">
+          <Link to="/" className="home-link">
+            &#60; Return to home
+          </Link>
+        </nav>
+        <div className="cart-item-grid">
+          {cartItems.map((item, index) => {
+            return <CartItem product={item} key={index} />;
+          })}
+        </div>
+      </main>
+      <div className="checkout">
+        <div>
+          <b className="checkout-total">TOTAL: ${setTotal()}</b>
+        </div>
+        <div>
+          <button className="button">CHECK OUT</button>
+        </div>
       </div>
-    </main>
+    </>
   );
 }
